2170df9cbb9de5108537dca54dcd7dd498dd4338
rajveerrajput09 <rajveerrajput09.2010@gmail.com>
2026-06-13 17:11:45 +0530

    i learn basics of sets here

---
+++ my learning/#13 sets in python.py
@@ -0,0 +1,23 @@
+# lerning the sets in python
+
+s = {2,333,444,"true",555,5,5,5,5,5,66,7,8,9}
+print(s)
+
+#They does not maitian order
+info= {"raj","don",19,19,True}
+print(info)
+
+raj = {1,2,True,'don',7,7,999}
+print(type(raj))
+
+d = set() #to make empty set
+print(type(d))
+
+#how to sccses the value
+for value in s:
+    print(value)
+
+
+
+
+
