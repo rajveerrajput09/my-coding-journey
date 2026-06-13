79d978925bd9ee805d2ce6ea6f3b0eda0a048beb
rajveerrajput09 <rajveerrajput09.2010@gmail.com>
2026-06-13 13:55:20 +0530

    introduction to tuples

---
+++ my learning/Tuples #24.py
@@ -0,0 +1,12 @@
+#Today i am learning tuples
+tup=(1,2,3,4,5,665,57,76,65,54,53,43,4)
+print(type(tup),tup)
+
+#tup[0]=23 #this will show you a error
+#tuples cannot be change
+
+#slicing in tuple
+tup2=tup[2::2]
+print(tup2)#this dont change the tuple but this makes new tuple
+
+#everything is same but they are immutable
