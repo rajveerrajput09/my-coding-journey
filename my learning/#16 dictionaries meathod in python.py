1de501d5ce58debd5db57f3cccec974235cdfcc5
rajveerrajput09 <rajveerrajput09.2010@gmail.com>
2026-06-13 18:31:26 +0530

    Some Dictionaries meathod

---
+++ my learning/#16 dictionaries meathod in python.py
@@ -0,0 +1,25 @@
+# dictionaries meathods
+
+ep1 ={1:"good",2:"bad",3:"worst",4:"fire him"}
+ep2 ={12:"very good",13:"exceptional",14:"very bad"}
+
+#update
+ep1.update(ep2)
+print(ep1)
+print(ep2)
+
+#clear
+ep1.clear()
+print(ep1)
+
+empt={}
+print(empt)
+
+#pop
+ep2.pop(12)
+print(ep2)
+
+#popitem
+ep2.popitem()
+print(ep2)
+
