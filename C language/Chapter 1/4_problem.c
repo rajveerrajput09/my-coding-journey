817befb9e4a9518544851d10980753cf19e6a92d
rajveerrajput09 <rajveerrajput09.2010@gmail.com>
2026-09-21 23:10:24 +0530

    convert the celcius to farenite in the c

---
+++ C language/Chapter 1/4_problem.c
@@ -0,0 +1,18 @@
+// Convert the celcious to farenhite
+
+#include <stdio.h>
+
+int main() {
+    int celcious;
+    float farenhinte;
+
+    printf("Entre the celcious: \n");
+    scanf("%d",&celcious);
+
+    farenhinte = (celcious*1.8) + 32;
+
+    printf("Therefore the farenhite is: %f",farenhinte);
+
+    
+    return 0;
+}
