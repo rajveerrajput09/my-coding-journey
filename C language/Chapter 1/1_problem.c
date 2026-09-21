24188887bbdc776c6535111c7ead971d7382bba5
rajveerrajput09 <rajveerrajput09.2010@gmail.com>
2026-09-21 23:08:12 +0530

    This is the first question
    Here i find the area of the rectangle in the c.

---
+++ C language/Chapter 1/1_problem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // this is the first problem, I am finding the area of a rectangle
+    int a = 2;
+    int b = 3;
+    int area = a*b;
+    printf("The area of the rectangle with sides %d and %d is: %d\n", a, b, area);
+    return 0;
+}
