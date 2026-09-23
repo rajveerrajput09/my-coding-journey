b64dc21b064d08f4a8ed4fe4c834ccad3d7b149c
rajveerrajput09 <rajveerrajput09.2010@gmail.com>
2026-09-23 17:19:31 +0530

    // THis is to cheack that if the number is 11
    Using the if for first time

---
+++ C language/Chapter 3/1_problem.c
@@ -0,0 +1,14 @@
+// THis is to cheack that if the number is 11
+
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    
+    if (a == 11)
+        printf("the number is 11");
+    else
+        printf("the number is not 11");      
+
+    return 0;
+}
